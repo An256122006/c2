4a5406703a668a8e2f9272cf59cb15d7c577990a
An256122006 <dinhtrongan2006@gmail.com>
2024-11-19 00:21:05 +0700

    Add files via upload

---
+++ bài 1 ss2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a=4; // kieu so nguyen
+	char b = a; // kieu ky tu
+	float c= 3.14; // kieu so thuc co do chinh xac don
+	double d = 3.16666; // kieu so thuc co do chinh xac kep
+	//in ra màn h?nh 
+	printf ("int a=%d\n",a);
+	printf ("char b=%c\n",b); 
+	printf ("float c=%f\n",c);
+	printf ("double b=%lf\n",d);
+	return 0; 
+	 
+ } 
