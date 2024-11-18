1f14a8ef373b1ea0f3d8605a84cd7cad899c4f8c
An256122006 <dinhtrongan2006@gmail.com>
2024-11-19 00:23:45 +0700

    Add files via upload

---
+++ bài 2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, c; 
+	printf ("moi nhap gia tri a:");
+	scanf ("%d" , &a);
+	printf  ("moi nhap gia tri b :");
+	scanf ("%d" , &b); 
+    c= (a + b); 
+printf ("tong la %d",c);	
+	return 0; 
+ } 
