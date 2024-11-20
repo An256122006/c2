f807dafd43ab8cc91ee1bb1a221fce10c34269b4
An256122006 <dinhtrongan2006@gmail.com>
2024-11-20 15:15:44 +0700

    Add files via upload

--- bài 6.cpp
+++ bài 6.cpp
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main()
 {
-	int r;
+	int r=2;
 	float pi=3.14, chu_vi;
-	printf ("moi nhap gia tri r:");
-	scanf ("%d" , &r);
 	chu_vi = 2*pi*r;
 	printf ("chu vi hinh tron la:%f", chu_vi);
 	return 0; 
