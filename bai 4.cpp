b1c22c81a24524a52377284bde14a73b58b4eba0
An256122006 <dinhtrongan2006@gmail.com>
2024-11-20 15:15:18 +0700

    Add files via upload

--- bai 4.cpp
+++ bai 4.cpp
@@ -1,8 +1,6 @@
 #include <stdio.h>
 int main(){
-	int a, chu_vi, dien_tich; 
-	printf ("moi nhap gia tri a:");
-	scanf ("%d" , &a);
+	int a=4, chu_vi, dien_tich; 
 	chu_vi = a*4;
 	dien_tich = a*a;
 	printf ("chu vi hinh vuong la:%d\n",chu_vi);
