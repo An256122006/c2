65972663fda9fb4baa1ceb59eb216ba421b6e27c
An256122006 <dinhtrongan2006@gmail.com>
2024-11-20 15:15:30 +0700

    Add files via upload

--- bài 5.cpp
+++ bài 5.cpp
@@ -1,13 +1,9 @@
 #include <stdio.h>
 int main(){
-	int a, b, chu_vi, dien_tich; 
-	printf ("moi nhap gia tri a:");
-	scanf ("%d" , &a);
-	printf ("moi nhap gia tri b:");
-	scanf ("%d" , &b);
+	int a=3, b=4, chu_vi, dien_tich;
 	chu_vi = (a+b)*2;
 	dien_tich = a*b;
 	printf ("chu vi hinh chu nhat la:%d\n",chu_vi);
-	printf ("dien tich hinh chu nhat la:%d\n",dien_tich);
+	printf ("dien tich hinh chu nhat  la:%d\n",dien_tich);
 return 0; 
 }
